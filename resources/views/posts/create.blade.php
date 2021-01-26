017fe52b7ef9ea917cfd7e29d46cc7581400a6c0
StepanNovikov96 <stepan.novikov.19962@gmail.com>
2021-01-26 10:37:22 +0300

    create post, pagination and search

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.layout')
+
+@section('content')
+    <form action="" method="post" enctype="multipart/form-data">
+        @csrf
+        <h3>Создать пост</h3>
+        <div class="form-group">
+            <input type="text" class="form-control" name="title" required>
+        </div>
+        <div class="form-group">
+            <textarea name="description" rows="10"  class="form-control" reqired></textarea>
+        </div>
+        <div class="form-group">
+            <input type="file">
+        </div>
+        <input type="submit" value="Создать пост" class="btn btn-outline-success">
+    </form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/','PostController@index');
+Route::get('post/', 'PostController@index')->name('post.index');
+Route::get('post/create', 'PostController@create')->name('post.create');
