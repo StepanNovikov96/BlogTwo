6fd547dc19b80ebd93d5bd9e623de017de62ec8e
StepanNovikov96 <stepan.novikov.19962@gmail.com>
2021-01-27 07:48:02 +0300

    update,delete post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.layout')
+@extends('layouts.layout',['title' =>"Пост №$post->post_id.$post->title"])
 
 @section('content')
     <div class="row">
@@ -7,6 +7,7 @@
                     <div class="card-header"><h2>{{$post->title}}</h2></div>
                     <div class="card-body">
                         <div class="card-img card-img_max" style="background-image: url({{$post->img?? asset('img/no-image.png')}})"></div>
+                        <div class="card-description">Описание поста: {{$post->description}}</div>
                         <div class="card-author">
                             Автор: {{$post->name}}
                         </div>
@@ -15,7 +16,11 @@
                         <div class="card-btn">
                             <a href="{{route('post.index')}}" class="btn btn-primary">На главную</a>
                             <a href="{{route('post.edit',['id' => $post->post_id])}}" class="btn btn-warning">Редактировать</a>
-                            <a href="{{route('post.destroy', ['id' => $post->post_id])}}" class="btn btn-danger">Удалить</a>
+                            <form action="{{route('post.destroy', ['id' => $post->post_id])}}" method="post" onsubmit="if(confirm('Точно удалить пост?')){return true;} else {return false;}">
+                                @csrf
+                                @method('DELETE')
+                                <input type="submit" class="btn btn-danger" value="Удалить">
+                            </form>
                         </div>
                     </div>
                 </div>
