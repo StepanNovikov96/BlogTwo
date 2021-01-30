4b58230f0f94255163d4f3a29ec630d029eb4688
StepanNovikov96 <stepan.novikov.19962@gmail.com>
2021-01-30 21:26:10 +0300

    PostRequest

--- resources/views/posts/parts/form.blade.php
+++ resources/views/posts/parts/form.blade.php
@@ -1,8 +1,8 @@
 <div class="form-group">
-    <input type="text" class="form-control" name="title" required value="{{$post->title??''}}">
+    <input type="text" class="form-control" name="title" required value="{{old('title')??$post->title??''}}">
 </div>
 <div class="form-group">
-    <textarea name="description" rows="10"  class="form-control" reqired>{{$post->description?? ''}}</textarea>
+    <textarea name="description" rows="10"  class="form-control" reqired>{{old('description')??$post->description?? ''}}</textarea>
 </div>
 <div class="form-group">
     <input type="file" name="img">
